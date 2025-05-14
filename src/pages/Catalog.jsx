a290ddd92e496aa20dc7363092fa2c28a725bc04
Dmitriy Shin <dimashin9@gmail.com>
2025-05-14 13:17:17 +0500

    updates

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -184,7 +184,7 @@ const ExportCatalog = () => {
 
 			// data -> iNav -> Nodes[2] -> Nodes[2]?.Facets -> Nodes[2]?.Facets[0]?.Refinements?.Nodes[0]?.Facets
 			const manufacturers =
-				data?.iNav?.Nodes[2]?.Facets[0]?.Refinements?.Nodes[0]?.Facets
+				data?.iNav?.Nodes[1]?.Facets[0]?.Refinements?.Nodes[0]?.Facets
 
 			setManufacturers(manufacturers)
 		}
